e5faacdcbc0784bb930bbca6875a113480712588
petras <petrasvestartas@gmail.com>
2025-11-19 22:58:05 +0100

    WIP tests

--- src/color_test.cpp
+++ src/color_test.cpp
@@ -1,22 +1,23 @@
-#include "catch_amalgamated.hpp"
-#include "color.h"
-#include "encoders.h"
-#include <filesystem>
+ #include "mini_test.h"
+ #include "color.h"
+ #include "encoders.h"
+ #include <filesystem>
 
-using namespace session_cpp;
+ using namespace session_cpp;
+ using namespace session_cpp::mini_test;
 
-TEST_CASE("Color JSON roundtrip", "[color]") {
-    Color original(100, 150, 200, 255);
-    original.name = "test_color";
-    
-    
-    encoders::json_dump(original, "test_color.json");
-    Color loaded = encoders::json_load<Color>("test_color.json");
+ MINI_TEST(Color, json_roundtrip) {
+   Color original(100, 150, 200, 255);
+   original.name = "test_color";
 
-    REQUIRE(loaded.r == original.r);
-    REQUIRE(loaded.g == original.g);
-    REQUIRE(loaded.b == original.b);
-    REQUIRE(loaded.a == original.a);
-    REQUIRE(loaded.name == original.name);    
-    std::filesystem::remove("test_color.json");
-}
+   encoders::json_dump(original, "test_color.json");
+   Color loaded = encoders::json_load<Color>("test_color.json");
+
+   MINI_CHECK(loaded.r == original.r);
+   MINI_CHECK(loaded.g == original.g);
+   MINI_CHECK(loaded.b == original.b);
+   MINI_CHECK(loaded.a == original.a);
+   MINI_CHECK(loaded.name == original.name);
+
+   std::filesystem::remove("test_color.json");
+ }
