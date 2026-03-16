05a98a9d3c09615e023ac1e42a77a047f251b85e
petrasvestartas <petrasvestartas@gmail.com>
2026-03-16 08:32:35 +0100

    ci

--- main_2.cpp
+++ main_2.cpp
@@ -44,10 +44,17 @@ int main() {
 
     // Remove
     mesh.remove_vertex(0);
+    mesh.remove_edge(1,2);
+    mesh.remove_face(0);
+    for (const auto& e : mesh.edges())
+        std::cout << e.first << " " << e.second << std::endl;
+    for (const auto& f : mesh.faces())
+        std::cout << f << std::endl;
     session.add_mesh(std::make_shared<Mesh>(mesh));
 
 
-    std::string fp = (std::filesystem::path(__FILE__).parent_path().parent_path() / "session_data" / "loft_debug.pb").string();
+    std::string fp = (std::filesystem::path(__FILE__).parent_path().parent_path() / "session_data" / "vertex_and_face_attributes.pb").string();
+    std::cout << "Dumping session to " << fp << std::endl;
     session.pb_dump(fp);
 
     return 0;
